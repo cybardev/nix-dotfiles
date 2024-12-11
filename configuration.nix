aed4bb2522f95ffe7f2d39d22c5eac37ab117913
cybardev <sheikh@cybar.dev>
2024-12-10 20:14:53 -0400

    re: increase terminal font size

--- configuration.nix
+++ configuration.nix
@@ -223,7 +223,7 @@
       settings = {
         shell = "zsh";
         font_family = "CaskaydiaCove Nerd Font Mono";
-        font_size = 12;
+        font_size = 14;
         enable_audio_bell = false;
         tab_bar_edge = "top";
       };
