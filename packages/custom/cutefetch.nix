b6e5abb6dda4b62bc98bcea7fe6b255c1081d6a9
cybardev <sheikh@cybar.dev>
2025-01-15 18:39:26 -0400

    re: use finalAttrs only instead of let

--- packages/custom/cutefetch.nix
+++ packages/custom/cutefetch.nix
@@ -5,47 +5,41 @@
   fetchFromGitHub,
 }:
 
-stdenvNoCC.mkDerivation (
-  finalAttrs:
-  let
-    src = fetchFromGitHub {
-      owner = "cybardev";
-      repo = "cutefetch";
-      rev = "e2462c64926f405f3c840efb37803def97c145ed";
-      hash = "sha256-DMp8tc1r5g3kHtboRp2xmx1o3Ze5UMqoYUHQwlT/gbI=";
-    };
-  in
-  {
-    inherit src;
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = finalAttrs.src.repo;
+  version = "0.3";
 
-    pname = src.repo;
-    version = "0.3";
+  src = fetchFromGitHub {
+    owner = "cybardev";
+    repo = "cutefetch";
+    rev = "e2462c64926f405f3c840efb37803def97c145ed";
+    hash = "sha256-DMp8tc1r5g3kHtboRp2xmx1o3Ze5UMqoYUHQwlT/gbI=";
+  };
 
-    nativeBuildInputs = [ pkgs.makeWrapper ];
+  nativeBuildInputs = [ pkgs.makeWrapper ];
 
-    installPhase = ''
-      runHook preInstall
-      chmod +x cutefetch
-      mkdir -p "$out/bin"
-      cp cutefetch "$out/bin/"
-      runHook postInstall
-    '';
+  installPhase = ''
+    runHook preInstall
+    chmod +x ${finalAttrs.src.repo}
+    mkdir -p "$out/bin"
+    cp ${finalAttrs.src.repo} "$out/bin/"
+    runHook postInstall
+  '';
 
-    postInstall = with pkgs; ''
-      wrapProgram "$out/bin/cutefetch" \
-        --prefix PATH : ${
-          lib.makeBinPath [ ]
-          ++ lib.optional pkgs.stdenvNoCC.hostPlatform.isLinux networkmanager xorg.xprop xorg.xdpyinfo
-        }
-    '';
+  postInstall = with pkgs; ''
+    wrapProgram "$out/bin/${finalAttrs.src.repo}" \
+      --prefix PATH : ${
+        lib.makeBinPath [ ]
+        ++ lib.optional pkgs.stdenvNoCC.hostPlatform.isLinux networkmanager xorg.xprop xorg.xdpyinfo
+      }
+  '';
 
-    meta = {
-      description = "Tiny coloured fetch script with cute little animals";
-      homepage = "https://github.com/${src.owner}/${src.repo}";
-      license = lib.licenses.gpl3Only;
-      maintainers = with lib.maintainers; [ ];
-      mainProgram = src.repo;
-      platforms = lib.platforms.all;
-    };
-  }
-)
+  meta = {
+    description = "Tiny coloured fetch script with cute little animals";
+    homepage = "https://github.com/${finalAttrs.src.owner}/${finalAttrs.src.repo}";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = finalAttrs.src.repo;
+    platforms = lib.platforms.all;
+  };
+})
