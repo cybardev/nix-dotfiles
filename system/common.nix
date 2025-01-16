5c0adb48965b2ec2a66c132044d1b687c3f3a39e
cybardev <sheikh@cybar.dev>
2025-01-16 01:14:17 -0400

    re: remove redundant block

--- system/common.nix
+++ system/common.nix
@@ -2,10 +2,4 @@
 {
   home-manager.backupFileExtension = "hm.bak";
   home-manager.useUserPackages = true;
-
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "vscode"
-    ];
 }
