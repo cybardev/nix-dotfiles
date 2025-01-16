cc919ad5d8c494c7ea51770e4e437fd189dde2fd
cybardev <sheikh@cybar.dev>
2025-01-16 11:15:05 -0400

    re: update installer script

--- install.sh
+++ install.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash -e
 
-git clone "https://github.com/cybardev/nixos-dotfiles.git" ~/.config/nixos
+NIXOS_CONFIG_DIR="$HOME/.config/nixos"
 
-sudo mv /etc/nixos /etc/nixos.bak
+git clone "https://github.com/cybardev/nixos-dotfiles.git" "$NIXOS_CONFIG_DIR"
 
-sudo ln -s "$HOME/.config/nixos" /etc/nixos
+sudo mv "/etc/nixos" "/etc/nixos.bak"
 
-mv ~/.config/nixos/hardware-configuration.nix ~/.config/nixos/hardware-configuration.nix.bak
-cp /etc/nixos.bak/hardware-configuration.nix ~/.config/nixos/
+sudo ln -s "$NIXOS_CONFIG_DIR" "/etc/nixos"
+
+mv "$NIXOS_CONFIG_DIR/system/hardware-configuration.nix" "$NIXOS_CONFIG_DIR/system/hardware-configuration.nix.bak"
+cp "/etc/nixos.bak/hardware-configuration.nix" "$NIXOS_CONFIG_DIR/system/"
 
 sudo nix-channel --add "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz" home-manager
 
 if [[ "$SURFACE_KERNEL" -eq 1 ]]; then
     sudo nix-channel --add "https://github.com/NixOS/nixos-hardware/archive/b12e314726a4226298fe82776b4baeaa7bcf3dcd.tar.gz" nixos-hardware
-    mv ~/.config/nixos/system.nix ~/.config/nixos/system.nix.bak
-    mv ~/.config/nixos/system-surface.nix ~/.config/nixos/system.nix
+    SURFACE_CONFIG="-I nixos-config=$NIXOS_CONFIG_DIR"
 fi
 
 sudo nix-channel --update
 
-sudo nixos-rebuild switch
+sudo nixos-rebuild switch $SURFACE_CONFIG
