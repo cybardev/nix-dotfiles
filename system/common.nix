d16bb5bf636549674a26a3fd5eeee60552020572
cybardev <sheikh@cybar.dev>
2025-01-15 20:05:58 -0400

    re: allow unfree vscode

--- system/common.nix
+++ system/common.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ lib, ... }:
 {
   home-manager.backupFileExtension = "hm.bak";
   home-manager.useUserPackages = true;
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode"
+    ];
 }
