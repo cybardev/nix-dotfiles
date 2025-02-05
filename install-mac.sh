40fdbbb45baf3ee41ad0b6a32567c3c321fc5244
Sheikh <sheikh@cybar.dev>
2025-02-05 12:52:46 -0400

    re: formatting

--- install-mac.sh
+++ install-mac.sh
@@ -8,8 +8,8 @@ git clone "https://github.com/cybardev/nixos-dotfiles.git" "$NIXOS_CONFIG_DIR"
 
 # add required package channels channels
 sudo -H nix-channel --add "https://nixos.org/channels/nixpkgs-24.11-darwin" nixpkgs
-sudo -H nix-channel --add "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz" home-manager
 sudo -H nix-channel --add "https://github.com/LnL7/nix-darwin/archive/nix-darwin-24.11.tar.gz" darwin
+sudo -H nix-channel --add "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz" home-manager
 sudo -H nix-channel --update
 
 # rebuild system from config
