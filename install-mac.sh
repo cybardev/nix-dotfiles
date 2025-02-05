0e6abbdddd123a21cd28f58f75d7ce9b075f2d77
Sheikh <sheikh@cybar.dev>
2025-02-05 12:40:49 -0400

    fix: missing darwin-config param

--- install-mac.sh
+++ install-mac.sh
@@ -6,11 +6,11 @@ NIXOS_CONFIG_DIR="$HOME/.config/nixos"
 # clone into nixos config directory
 git clone "https://github.com/cybardev/nixos-dotfiles.git" "$NIXOS_CONFIG_DIR"
 
-# add home-manager channel
+# add home-manager and nix-darwin channels
 sudo -H nix-channel --add "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz" home-manager
-
-# update nix channels
+sudo -H nix-channel --add https://github.com/LnL7/nix-darwin/archive/nix-darwin-24.11.tar.gz darwin
 sudo -H nix-channel --update
 
 # rebuild system from config
-sudo -H darwin-rebuild switch -I "$NIXOS_CONFIG_DIR/configuration-darwin.nix"
+nix-build '<darwin>' -A darwin-rebuild
+./result/bin/darwin-rebuild switch -I "darwin-config=$NIXOS_CONFIG_DIR/configuration-darwin.nix"
