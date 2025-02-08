2883e49aca73f91b2e4cd03e764b10f7d61162fe
Sheikh <sheikh@cybar.dev>
2025-02-08 01:07:14 -0400

    re: formatting

--- packages/darwin.nix
+++ packages/darwin.nix
@@ -23,9 +23,9 @@
     enable = true;
     onActivation.cleanup = "zap";
     masApps = {
-      "OwlFiles" = 510282524;
-      "MuteKey" = 1509590766;
       "KeyNote" = 409183694;
+      "MuteKey" = 1509590766;
+      "OwlFiles" = 510282524;
     };
     brews = [
       "weasyprint"
@@ -37,9 +37,9 @@
       "font-fira-code-nerd-font"
       "font-jetbrains-mono-nerd-font"
       "font-open-dyslexic-nerd-font"
-      "karabiner-elements"
       "gb-studio"
       "hiddenbar"
+      "karabiner-elements"
       "keyclu"
       "mark-text"
       "sonic-pi"
