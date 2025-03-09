362fcf4c34c38f3e2c05c300e66c7f80e367d703
cybardev <sheikh@cybar.dev>
2025-03-09 01:15:02 -0300

    re: formatting

--- packages/config/utils.nix
+++ packages/config/utils.nix
@@ -1,64 +1,66 @@
 { pkgs, ... }:
 {
-  programs.bat = {
-    enable = true;
-    config.theme = "OneHalfDark";
-  };
-
-  programs.eza = {
-    enable = true;
-    colors = "auto";
-    git = true;
-    icons = "auto";
-    extraOptions = [
-      "--group-directories-last"
-      "--sort=extension"
-    ];
-  };
-
-  programs.bottom = {
-    enable = true;
-    settings.styles.theme = "nord";
-  };
-
-  programs.cava = {
-    enable = true;
-    settings = {
-      general = {
-        bars = 0;
-        bar_width = 6;
-        bar_spacing = 2;
-      };
-      color = {
-        gradient = 1;
-        gradient_count = 2;
-        gradient_color_1 = "'#bb7ebb'";
-        gradient_color_2 = "'#ffbeff'";
-      };
-      output.method = "ncurses";
-      smoothing.gravity = 42;
+  programs = {
+    bat = {
+      enable = true;
+      config.theme = "OneHalfDark";
     };
-  };
 
-  programs.poetry = {
-    enable = true;
-    settings.virtualenvs.in-project = true;
-  };
-
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
-    userName = "cybardev";
-    userEmail = "sheikh@cybar.dev";
-    extraConfig = {
-      init.defaultBranch = "main";
-      credential.helper = "store";
-      pull.rebase = false;
+    eza = {
+      enable = true;
+      colors = "auto";
+      git = true;
+      icons = "auto";
+      extraOptions = [
+        "--group-directories-last"
+        "--sort=extension"
+      ];
     };
-  };
 
-  programs.gitui = {
-    enable = true;
-    theme = ./gitui-catppuccin.ron;
+    bottom = {
+      enable = true;
+      settings.styles.theme = "nord";
+    };
+
+    cava = {
+      enable = true;
+      settings = {
+        general = {
+          bars = 0;
+          bar_width = 6;
+          bar_spacing = 2;
+        };
+        color = {
+          gradient = 1;
+          gradient_count = 2;
+          gradient_color_1 = "'#bb7ebb'";
+          gradient_color_2 = "'#ffbeff'";
+        };
+        output.method = "ncurses";
+        smoothing.gravity = 42;
+      };
+    };
+
+    poetry = {
+      enable = true;
+      settings.virtualenvs.in-project = true;
+    };
+
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+      userName = "cybardev";
+      userEmail = "sheikh@cybar.dev";
+      extraConfig = {
+        init.defaultBranch = "main";
+        credential.helper = "store";
+        pull.rebase = false;
+      };
+    };
+
+    gitui = {
+      enable = true;
+      theme = ./gitui-catppuccin.ron;
+    };
   };
 }
