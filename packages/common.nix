cb2e9dad9b7ddd6d2578a1a724c4f2e65aea0f60
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-19 16:27:10 -0300

    re: formatting

--- packages/common.nix
+++ packages/common.nix
@@ -16,48 +16,53 @@ in
   ];
 
   home = {
-    packages = with pkgs; [
-      # cypkgs
-      cypkgs.cutefetch
-      cypkgs.jitterbugpair
-      # cypkgs.freej2me
-      cypkgs.ytgo
-
-      # nixpkgs
-      python3Packages.ptpython
-      nixfmt-rfc-style
-      nix-search-cli
-      gnome-mahjongg
-      signal-desktop
-      imagemagick
-      lazydocker
-      lunarvim
-      babashka
-      dfu-util
-      visidata
-      inkscape
-      poppler
-      cmatrix
-      openssl
-      zoom-us
-      thonny
-      ccache
-      p7zip
-      cmake
-      ninja
-      bruno
-      brave
-      gimp
-      ncdu
-      nixd
-      hugo
-      wget
-      unar
-      go
-      # love
-      luajit
-      luajitPackages.luarocks
-    ];
+    packages = builtins.attrValues {
+      inherit (cypkgs)
+        cutefetch
+        jitterbugpair
+        # freej2me
+        ytgo
+        ;
+      inherit (pkgs)
+        nixfmt-rfc-style
+        nix-search-cli
+        gnome-mahjongg
+        signal-desktop
+        imagemagick
+        lazydocker
+        lunarvim
+        babashka
+        dfu-util
+        visidata
+        inkscape
+        poppler
+        cmatrix
+        openssl
+        zoom-us
+        thonny
+        ccache
+        p7zip
+        cmake
+        ninja
+        bruno
+        brave
+        gimp
+        ncdu
+        nixd
+        hugo
+        wget
+        unar
+        go
+        # love
+        luajit
+        ;
+      inherit (pkgs.python3Packages)
+        ptpython
+        ;
+      inherit (pkgs.luajitPackages)
+        luarocks
+        ;
+    };
 
     file = {
       ".config/lvim/config.lua".source = ./config/lvim.lua;
