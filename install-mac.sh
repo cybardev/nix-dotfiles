bc0894c0b724cef7592eb6df82649885b78ed030
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-20 21:41:47 -0300

    feat: update install scripts to flake-based setup

--- install-mac.sh
+++ install-mac.sh
@@ -6,13 +6,5 @@ NIXOS_CONFIG_DIR="$HOME/.config/nixos"
 # clone into nixos config directory
 git clone "https://github.com/cybardev/nix-dotfiles.git" "$NIXOS_CONFIG_DIR"
 
-# add required Nix channels
-sudo -H nix-channel --add "https://nixos.org/channels/nixpkgs-24.11-darwin" nixpkgs
-sudo -H nix-channel --add "https://github.com/LnL7/nix-darwin/archive/nix-darwin-24.11.tar.gz" darwin
-sudo -H nix-channel --add "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz" home-manager
-sudo -H nix-channel --add "https://github.com/cybardev/nix-channel/archive/main.tar.gz" cypkgs
-sudo -H nix-channel --update
-
 # rebuild system from config
-nix-build "<darwin>" -A darwin-rebuild
-./result/bin/darwin-rebuild switch -I "darwin-config=$NIXOS_CONFIG_DIR/configuration-darwin.nix"
+nix run nix-darwin/master#darwin-rebuild -- switch --flake "$NIXOS_CONFIG_DIR#darwin"
