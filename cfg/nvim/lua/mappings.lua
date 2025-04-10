880c9589a13164f2e8388f25ed5e792688ae1978
Sheikh <50134239+cybardev@users.noreply.github.com>
2025-04-10 11:10:20 -0300

    re: formatting

--- cfg/nvim/lua/mappings.lua
+++ cfg/nvim/lua/mappings.lua
@@ -5,8 +5,6 @@ local map = vim.keymap.set
 -- add yours here
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-
 map({ "n", "t" }, "<leader>`", function()
   require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
 end, { desc = "terminal toggle floating term" })
-
