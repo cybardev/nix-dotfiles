a05f6b4d292f2019179854c157c8e4536304db38
Sheikh <50134239+cybardev@users.noreply.github.com>
2025-04-10 12:52:11 -0300

    fix: update config options

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -189,7 +189,7 @@ in
   services.printing.enable = false;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
