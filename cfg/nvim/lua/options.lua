f4cf0898b68c65d3e6a45859b38b499bd8ea791c
Sheikh <50134239+cybardev@users.noreply.github.com>
2025-04-10 01:57:00 -0300

    feat: add relative line nums to nvim

--- cfg/nvim/lua/options.lua
+++ cfg/nvim/lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
+local o = vim.o
+
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt ="both" -- to enable cursorline!
+o.relativenumber = true
