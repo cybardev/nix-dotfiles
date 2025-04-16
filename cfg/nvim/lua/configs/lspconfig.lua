f95d41d101c11f3a5aa2e41ccee5f265cbacfbf1
cybardev <50134239+cybardev@users.noreply.github.com>
2025-04-15 23:33:21 -0300

    feat: use alejandra as default nix formatter

--- cfg/nvim/lua/configs/lspconfig.lua
+++ cfg/nvim/lua/configs/lspconfig.lua
@@ -23,8 +23,15 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.nixd.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    nixd = {
+      formatting = {
+        command = { "alejandra" },
+      },
+    },
+  },
+}
