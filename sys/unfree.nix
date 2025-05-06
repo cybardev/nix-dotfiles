55a118b80745563ab1f2c281043d4b6eeb88024a
cybardev <50134239+cybardev@users.noreply.github.com>
2025-05-06 13:57:51 -0300

    fix: update unfree package name

--- sys/unfree.nix
+++ sys/unfree.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
-      "signal-desktop"
+      "signal-desktop-bin"
       "lunarclient"
       "freej2me"
       "aseprite"
