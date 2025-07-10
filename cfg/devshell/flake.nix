7a157e7020628250fe27ed91544a25b0d0473b75
cybardev <50134239+cybardev@users.noreply.github.com>
2025-07-10 02:53:21 -0300

    feat: add flake-based devshell example

---
+++ cfg/devshell/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Flake-based Nix devshell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: f { pkgs = import nixpkgs { inherit system; }; }
+        );
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs }:
+        {
+          default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+    };
+}
