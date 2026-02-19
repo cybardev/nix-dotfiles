3eac035812821453ce3ebe1733053ef3c3f77984
cybardev <50134239+cybardev@users.noreply.github.com>
2026-02-19 16:06:36 -0400

    chore: avoid building mpv custom package

--- pkg/common.nix
+++ pkg/common.nix
@@ -255,19 +255,19 @@ in
 
     mpv = {
       enable = true;
-      package = (
-        pkgs.mpv.override {
-          mpv-unwrapped = pkgs.mpv-unwrapped.override {
-            ffmpeg = pkgs.ffmpeg-full;
-          };
-          youtubeSupport = true;
-          scripts = with pkgs.mpvScripts; [
-            sponsorblock-minimal
-            visualizer
-            uosc
-          ];
-        }
-      );
+      # package = (
+      #   pkgs.mpv.override {
+      #     mpv-unwrapped = pkgs.mpv-unwrapped.override {
+      #       ffmpeg = pkgs.ffmpeg-full;
+      #     };
+      #     youtubeSupport = true;
+      #     scripts = with pkgs.mpvScripts; [
+      #       sponsorblock-minimal
+      #       visualizer
+      #       uosc
+      #     ];
+      #   }
+      # );
     };
 
     poetry = {
