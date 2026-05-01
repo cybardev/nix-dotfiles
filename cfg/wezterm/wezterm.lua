249d4ea8f1845eef37c5b8b1f2639af9fb6c9062
cybardev <50134239+cybardev@users.noreply.github.com>
2026-05-01 11:08:26 -0300

    wezterm: use config_builder
    better debug logs

--- cfg/wezterm/wezterm.lua
+++ cfg/wezterm/wezterm.lua
@@ -1,44 +1,45 @@
-local wezterm = require 'wezterm'
-local act = wezterm.action
+local wez = require 'wezterm'
+local cfg = wez.config_builder()
+local act = wez.action
+-- CONFIG START --
 
-local config = {
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-  tab_bar_at_bottom = true,
+cfg.use_fancy_tab_bar = false
+cfg.hide_tab_bar_if_only_one_tab = true
+cfg.tab_bar_at_bottom = true
 
-  window_background_opacity = 0.86,
-  color_scheme = 'Kanagawa Dragon (Gogh)',
+cfg.window_background_opacity = 0.86
+cfg.color_scheme = 'Kanagawa Dragon (Gogh)'
 
-  font = wezterm.font 'CaskaydiaCove Nerd Font Mono',
-  font_size = 14,
+cfg.font = wez.font 'CaskaydiaCove Nerd Font Mono'
+cfg.font_size = 14
 
-  keys = {
-    {
-      key = 'Enter',
-      mods = 'SUPER',
-      action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = '[',
-      mods = 'SUPER',
-      action = act.ActivatePaneDirection 'Prev',
-    },
-    {
-      key = ']',
-      mods = 'SUPER',
-      action = act.ActivatePaneDirection 'Next',
-    },
-    {
-      key = 'w',
-      mods = 'SUPER',
-      action = act.CloseCurrentPane { confirm = false },
-    },
-    {
-      key = 'W',
-      mods = 'SUPER',
-      action = act.CloseCurrentTab { confirm = false },
-    },
+cfg.keys = {
+  {
+    key = 'Enter',
+    mods = 'SUPER',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '[',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Prev',
+  },
+  {
+    key = ']',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Next',
+  },
+  {
+    key = 'w',
+    mods = 'SUPER',
+    action = act.CloseCurrentPane { confirm = false },
+  },
+  {
+    key = 'W',
+    mods = 'SUPER',
+    action = act.CloseCurrentTab { confirm = false },
   },
 }
 
-return config
+-- CONFIG END --
+return cfg
