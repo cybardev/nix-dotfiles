a2fd7d916e76ead2dc5bb675dd2e5a82dabdf4c3
cybardev <50134239+cybardev@users.noreply.github.com>
2026-06-17 20:07:37 -0300

    feat: add fingerprint option

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -258,6 +258,8 @@ in
 
   hardware.bluetooth.enable = true;
 
+  services.fprintd.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
